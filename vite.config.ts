cbb6cf867dd55b5da750e89db63b2797543a64ec
SilverBackDev <olalekanisaac75@gmail.com>
2025-08-13 04:41:50 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/bigizic.github.io' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/bigizic.github.io' : '/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
